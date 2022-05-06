1965821a47947bfbed3d947aa78879f603999bd3
Daniel Selorm Ocloo <ynnadocloo@gmail.com>
2022-05-06 13:18:08 +0000

    deleted notecardata class and placed it's contents in NoteCard class

--- lib/notecard.dart
+++ lib/notecard.dart
@@ -14,63 +14,76 @@ class NoteCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 190,
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      decoration: BoxDecoration(
-        color: Colors.grey.withOpacity(0.1),
-        border: Border.all(
-          width: 1,
-          color: Colors.grey.withOpacity(0.5),
-        ),
-        borderRadius: BorderRadius.circular(10),
+    return GridView.builder(
+      itemCount: 10,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 15,
+        mainAxisSpacing: 15,
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        //this didn't seem to have any effect
-        //mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Row(
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 100,
+          width: 190,
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.1),
+            border: Border.all(
+              width: 1,
+              color: Colors.grey.withOpacity(0.5),
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            //this didn't seem to have any effect
+            //mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              const Icon(
-                Icons.circle_rounded,
-                color: Colors.purple,
-                size: 10,
+              Row(
+                children: <Widget>[
+                  const Icon(
+                    Icons.circle_rounded,
+                    color: Colors.purple,
+                    size: 10,
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    '$title',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(width: 10),
+              const SizedBox(height: 14),
               Text(
-                '$title',
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
+                '$body',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontWeight: FontWeight.w600,
+                  fontSize: 12,
+                ),
+              ),
+              const Spacer(),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  '$lastedited',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.3),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 10,
+                  ),
                 ),
               ),
             ],
           ),
-          const SizedBox(height: 14),
-          Text(
-            '$body',
-            style: TextStyle(
-              color: Colors.black.withOpacity(0.5),
-              fontWeight: FontWeight.w600,
-              fontSize: 12,
-            ),
-          ),
-          const Spacer(),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Text(
-              '$lastedited',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.3),
-                fontWeight: FontWeight.w500,
-                fontSize: 10,
-              ),
-            ),
-          ),
-        ],
-      ),
+        );
+      },
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      primary: false,
     );
   }
 }
